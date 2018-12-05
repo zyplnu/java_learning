255a8f1e883c4715845011c95d97fbdc58f8d7ac
zyplnu <429510037@qq.com>
2018-12-05 10:57:58 +0800

    add code

--- src/main/java/edu/lnu/Test.java
+++ src/main/java/edu/lnu/Test.java
@@ -5,6 +5,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("success");
         System.out.println("fix code");
+        System.out.println("fix code 2");
     }
 
 }
