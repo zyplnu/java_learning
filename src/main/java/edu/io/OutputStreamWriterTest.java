bb6b13679c70b0f9a4514e50e83c094e80afa218
zyplnu <429510037@qq.com>
2019-01-26 18:09:06 +0800

    add new class

--- src/main/java/edu/io/OutputStreamWriterTest.java
+++ src/main/java/edu/io/OutputStreamWriterTest.java
@@ -4,12 +4,57 @@ import java.io.*;
 
 public class OutputStreamWriterTest {
 
-    public static void main(String[] args) throws IOException {
-        OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream("D:/test.txt") , "UTF-8");
+    private void readFile(String fileName) throws IOException {
+        readFile(fileName , null);
+    }
+
+    private void readFile(String fileName , String charsetName) throws IOException {
+        InputStream is = new FileInputStream(fileName);
+        InputStreamReader isr;
+        if(charsetName != null){
+            isr = new InputStreamReader(is , charsetName);
+        }else {
+            isr = new InputStreamReader(is);
+        }
+        BufferedReader br = new BufferedReader(isr);
+        String data;
+        while((data = br.readLine()) != null){
+            System.out.println(data);
+        }
+        br.close();
+    }
+
+    private void copyFile(String from , String fromCharset , String to , String toCharset) throws IOException {
+        InputStream is = new FileInputStream(from);
+        InputStreamReader isr;
+        if(fromCharset != null){
+            isr = new InputStreamReader(is , fromCharset);
+        }else {
+            isr = new InputStreamReader(is);
+        }
+        BufferedReader br = new BufferedReader(isr);
+
+
+        OutputStream os = new FileOutputStream(to);
+        OutputStreamWriter osw = new OutputStreamWriter(os , toCharset);
         BufferedWriter bw = new BufferedWriter(osw);
-        char c = '好';
-        osw.write(c);
-        osw.close();
+        PrintWriter pw = new PrintWriter(bw , true);
+
+        String data;
+        while((data = br.readLine()) != null){
+            pw.println(data);
+        }
+        br.close();
+        pw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        OutputStreamWriterTest test = new OutputStreamWriterTest();
+        test.readFile("D:/test.txt" +
+                "");
+        test.copyFile("D:/test.txt" , null , "D:/out.txt" , "UTF-8");
+        test.readFile("D:/out.txt");
+        test.readFile("D:/out.txt" , "GBK");
     }
 
 }
